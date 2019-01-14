54ed4ae33350f0af9c5e5ed9406a90becea6440d
Mats Johan Pedersen <MatsPedersen@protonmail.com>
2019-01-14 14:57:08 +0100

    Added more tests and reverse engineered more packets

--- data_parsers.py
+++ data_parsers.py
@@ -1,17 +1,65 @@
 import struct
 from enum import Enum
 
+global_packet_id = None
+global_packet_origin = None
 
 def parse(origin, port, data):
-    if origin == "server" or port == 3333:
-        return
-    print("Message from {} on port {}: {}".format(origin, port, data.hex()))
+    global global_packet_id
+    global global_packet_origin
     packet_id = struct.unpack(">H", data[:2])[0]
-    print(hex(packet_id))
+    global_packet_id = packet_id
+    global_packet_origin = origin
+    if packet_id == 0 or port == 3333 or packet_id == PacketType.Position.value or packet_id == PacketType.Jump.value:
+        return
+    #print("Message from {} on port {}: {}".format(origin, port, data.hex()))
+    handler.get(packet_id, h_noop)(data[2:])
 
 
-def position_parser():
-    pass
+def h_noop(data):
+    global global_packet_id
+    global global_packet_origin
+    print("Unknown {} packet: {} {}".format(global_packet_origin, format(global_packet_id, 'x'), data.hex()))
+
+
+def h_position(data):
+    x, y, z = struct.unpack("fff", data[0:3*4])
+    print("Position: {:.2f} / {:.2f} / {:.2f}".format(x, y, z))
+
+
+def h_jump(data):
+    print("Jump: {}".format(data.hex()[6:len(data.hex()) - 16]))
+
+
+def h_server_talk(data):
+    print("Server talk: {}".format(data.hex()))
+
+
+def h_client_talk(data):
+    print("Client talk: {}".format(data.hex()))
+
+def h_client_initiate_conversation(data):
+    print("Client initiate conversation: {}".format(data.hex()))
+
+def h_server_end_conversation(data):
+    print("Server end conversation: {}".format(data.hex()))
+
+def h_sprint_toggle(data):
+    print("Sprint toggle: {}".format(data.hex()))
+
+def h_enemy_position(data):
+    print("Enemy position: {}".format(data.hex()))
+
+handler = {
+    0x6d76: h_position,
+    0x6a70: h_jump,
+    0x2373: h_server_talk,
+    0x233e: h_client_talk,
+    0x6565: h_client_initiate_conversation,
+    0x2366: h_server_end_conversation,
+    0x726e: h_sprint_toggle,
+    0x7073: h_enemy_position
+}
 
 
 def get_packet_id(data):
@@ -19,45 +67,40 @@ def get_packet_id(data):
 
 
 class PacketType(Enum):
-    Position = "6d76"
+    """"
+    [2 byte packet id:0x6d76]
+    [4 byte x coords]
+    [4 byte y coords]
+    [4 byte z coords]
+    [4 byte camera movement]
+    [2 byte ?]
+    [2 byte key input]"""
+    Position = int(0x6d76)
+    """
+    [2 byte packet id:0x6a70]
+    [1 byte space key press]
+    [2 byte position packet id?: 0x6d76]
+    [4 byte x coords]
+    [4 byte y coords]
+    [4 byte z coords]
+    [4 byte camera movement]
+    [2 byte ?]
+    [2 byte key input]"""
+    Jump = int(0x6a70)
+    Server_talk = int(0x2373)
+    Client_talk = int(0x233e)
+    Client_initiate_conversation = int(0x6565)
+    Server_end_conversation = int(0x2366)
+    Sprint_toggle = int(0x726e)
+    Enemy_position = int(0x7073)
 
 """
-Packet dump from jumping:
-6d76 05fa13c7 6fd69ec6 4ef72345 d0fa4918 0000 0000
-6d76 05fa13c7 6fd69ec6 85042545 d0fa4918 0000 0000
-6d76 05fa13c7 6fd69ec6 7fc32545 d0fa4918 0000 0000
-6d76 05fa13c7 6fd69ec6 29572645 d0fa4918 0000 0000
-6d76 05fa13c7 6fd69ec6 5a312645 d0fa4918 0000 0000
-6d76 05fa13c7 6fd69ec6 00c22545 d0fa4918 0000 0000
-6d76 05fa13c7 6fd69ec6 f5092545 d0fa4918 0000 0000
-6d76 05fa13c7 6fd69ec6 d5ff2345 d0fa4918 0000 0000
-6d76 05fa13c7 6fd69ec6 a5ac2245 d0fa4918 0000 0000
-6d76 05fa13c7 6fd69ec6 1c092145 d0fa4918 0000 0000
-                       
 
-Packet dump from moving:
-6d76 727e13c7 fdf09fc6 d8c81b45 e6fa8918 0000 7f7f
-6d76 298c13c7 65c69fc6 69c51b45 e6fa8918 0000 7f7f
-6d76 198f13c7 b8889fc6 acc41b45 e6fa8918 0000 7f00
-6d76 6f8013c7 7f519fc6 54c81b45 e6fa8918 0000 7f81
-6d76 2b6813c7 95419fc6 65ce1b45 e6fa8918 0000 0081
-6d76 0f4f13c7 b9569fc6 aed41b45 e6fa8918 0000 0081
-6d76 993a13c7 c8879fc6 87d71b45 e6fa8918 0000 8181
-6d76 7e3613c7 26c99fc6 87d71b45 e6fa8918 0000 817f
-6d76 e74613c7 19f39fc6 3dd71b45 e6fa8918 0000 817f
-6d76 1d6113c7 bcfe9fc6 aed01b45 e6fa8918 0000 007f
-
-Packet dump from moving camera:
-6d76 4f7a13c7 eeec9fc6 60ca1b45 740d81fe 0000 0000
-6d76 4f7a13c7 eeec9fc6 60ca1b45 ddf8f340 0000 0000
-6d76 4f7a13c7 eeec9fc6 60ca1b45 91164643 0000 0000
-6d76 4f7a13c7 eeec9fc6 60ca1b45 f0168510 0000 0000
-6d76 4f7a13c7 eeec9fc6 60ca1b45 70f43734 0000 0000
-6d76 4f7a13c7 eeec9fc6 60ca1b45 88094f58 0000 0000
-6d76 4f7a13c7 eeec9fc6 60ca1b45 0f1ff127 0000 0000
-6d76 4f7a13c7 eeec9fc6 60ca1b45 e401d621 0000 0000
-6d76 4f7a13c7 eeec9fc6 60ca1b45 cd07344b 0000 0000
-
-Packet format:
-[2 byte packet id][4 byte x coords][4 byte y coords][4 byte z coords][4 byte camera movement][2 byte ?][2 byte key input]
+2 byte packet id     1 byte keypress                constant
+                     1 for press, 0 for release     Position packet?  x            y              z            camera position       ?      Key pressed
+Jump:                   00                          6d76        687150c7        58eb60c7        c5719444    78f55108            0000        0000
+Jump:                   01                          6d76        687150c7        58eb60c7        e8da8144    78f55108            0000        0000
+Jump:                   00                          6d76        687150c7        58eb60c7        a6d79344    78f55108            0000        0000
+                                                                044f50c7        722361c7        f5bf8544    54f3dc19            0000        0000
+                        00                          6d76        2f991bc7        5fec9ac6        d41a2745    dff26b8a            0000        0000
 """
